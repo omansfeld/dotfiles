1ee1077b053162b03342895b5f80ea07111fa233
mansf-osk <mansfeld.oskar@gmail.com>
2024-02-23 11:23:52 +0100

    nvim: installed lazyvim and jj as escape

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,2 @@
-if vim.g.vscode then
-    -- VSCode extension
-    print("loaded vscode nvim config")
-else
-    -- ordinary Neovim
-    local options = { noremap = true }
-    vim.keymap.set("i", "jj", "<Esc>", options)
-    print("loaded ordinary nvim config")
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
